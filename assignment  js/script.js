d315c8aa58f6a96df81c5fd00a79a98b3609d133
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-06-19 04:57:15 -0700

    Add files via upload

---
+++ assignment  js/script.js
@@ -0,0 +1,9 @@
+// alert("Most welcome to our website")
+// alert("Error! Please enter a valid password")
+// alert("Welcome to JS land... \n Happy coding!")
+// alert("Welcome to JS land ")
+// alert("Happy Coding \n Prevent this page from creating additional dialogs" )
+// alert("Hello... I can run JS through my web browser's console" )
+
+
+console.log("eshal");
